0f28d4747e7e378b26a509a20d3dfefaaea4db62
H.Salari <H.Salari@sadad.co.ir>
2024-02-24 09:25:16 +0330

    #init-add-uaa-microservice #init-add-common-microservice

---
+++ ms-uaa/src/main/java/org/_3HCompany/microservices/UAAApplication.java
@@ -0,0 +1,12 @@
+package org._3HCompany.microservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UAAApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UAAApplication.class, args);
+    }
+}
